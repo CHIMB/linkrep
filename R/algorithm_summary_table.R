393e7c13d3438444f83e393c4bbeacf788883d64
stoughty111 <estoughton111@gmail.com>
2024-08-15 12:02:22 -0500

    Updated roxygen documentation algorithm_summary_table

--- R/algorithm_summary_table.R
+++ R/algorithm_summary_table.R
@@ -4,15 +4,15 @@
 #'
 #' @param data A data frame that data contains information on the linkage algorithm
 #'  (ex. linking variables on each pass).
-#' @param output_formatString specifying the desired output format. Allowed values
+#' @param output_format String specifying the desired output format. Allowed values
 #'  are "\code{pdf}" or "\code{docx}".
 #' @param font_size A number specifying the font size for the table text.
 #'  Default is \code{12}.
 #' @param font_style A string specifying the font style. Must be present in
 #'  \code{system_fonts()$name} or \code{system_fonts()$family}. See \code{\link[systemfonts]{system_fonts}}
 #'  for more details.
-#' @param footnotes A character vector of additional footnotes for
-#' the table. Each element in the vector will be displayed on a new line.
+#' @param footnotes A character vector of additional footnotes for the algorithm
+#' summary table. Each element in the vector will be displayed on a new line.
 #' @param thousands_separator A string specifying the style of the
 #'  thousands separator in all numeric values. Default is "\code{,}".
 #' @param decimal_mark A string specifying the style of the decimal mark
@@ -20,8 +20,7 @@
 #' @param num_decimal_places A number specifying the number of digits to output
 #'  after the decimal mark of all necessary numeric values. Default is \code{1}.
 #'
-#' @return A \code{flextable}.
-#' @export
+#' @return A \code{flextable} object.
 #'
 algorithm_summary_table <- function(data,
                                     output_format,
