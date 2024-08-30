d6a703caf2f2dbd0474cec99cb1e4898853e09ad
stoughty111 <estoughton111@gmail.com>
2024-08-30 12:43:00 -0500

    Removed redundant tests

--- tests/testthat/test-abbreviation_table.R
+++
@@ -1,8 +0,0 @@
-test_that("Testing display_headers updates the table", {
-  data <- data.table(abbrev = c("idk", "fyi"), descr = c("i don't know", "for your information"))
-  ft <- abbreviation_table(data, "docx", display_headers = TRUE)
-  expect_equal(length(ft$header$content$data), 2)
-
-  ft <- abbreviation_table(data, "docx", display_headers = FALSE)
-  expect_equal(length(ft$header$content$data), 0)
-})
